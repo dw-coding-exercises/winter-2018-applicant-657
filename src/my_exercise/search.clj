9490c026b078ced786f223aebf78e1a1a1fe37c4
Anonymous Applicant <anonymous@example.com>
2018-02-11 16:43:16 -0700

    Try to get the params from the form into a results section, can get the type of request

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -7,14 +7,30 @@
    [:meta {:charset "UTF-8"}]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1.0, maximum-scale=1.0"}]
-   [:title "I am a search page!"]
+   [:title "Search"]
    [:link {:rel "stylesheet" :href "default.css"}]])
 
-(defn instructions [request]
-  [:div {:class "instructions"}
-   [:h2 "Current elections"]])
+(defn search-header [request]
+  [:div {:class "search-header"}
+   [:h1 "Search results for elections"]
+   [:p "You are in "]])
+
+(defn display-result [request]
+  (anti-forgery-field)
+
+  ; learned the following from: https://gist.github.com/zehnpaard/665edf183818b4df707b5f0535ecdc0c
+  (let [{:keys [params uri]} request
+        param-name (get params "name")
+        request-type (if (= uri "/get-submit") "GET" "POST")]
+    (str
+     "<div>
+        <h1>Hello " param-name "!</h1>
+        <p>Submitted via a " request-type " request.</p>
+        <p><a href='..'>Return to main page</p>
+      </div>")))
 
 (defn page [request]
   (html5
    (header request)
-   (instructions request)))
+   (search-header request)
+   (display-result request)))
